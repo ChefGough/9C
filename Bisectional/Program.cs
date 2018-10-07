64afb04d5850d83baaaea8e2226a6aa7a408cbd5
Alexander Gough <gougha3@my.erau.edu>
2018-10-07 14:11:02 -0400

    9C

---
+++ Bisectional/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bisectional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guesses = 0;
+            Random random = new Random();
+            int num = random.Next(1, 100);
+            Console.WriteLine("Pick between 1 and 100");
+
+            int i = 0;
+
+            while (guesses != num)
+            {
+                try
+                {
+                    guesses = Convert.ToInt32(Console.ReadLine());
+
+                    if (guesses > num)
+                    {
+                        Console.WriteLine("Guess too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Guess too low");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Your entry must be a whole number.");
+                }
+
+                i++;
+            }
+            Console.WriteLine("That is correct! You needed " + i + " guesses to get the correct number");
+            Console.ReadLine();
+            
+        }
+        
+        
+    }
+}
